11f6b36233c957a64edf2c34a4835f613053f734
unknown <1224119154@qq.com>
2022-03-17 18:14:37 +0800

    【初始化】

---
+++ pages/cover/cover.js
@@ -0,0 +1,83 @@
+// pages/cover/cover.js
+Page({
+
+  login(){
+    wx.getUserProfile({
+      desc: '必须进行授权',
+      success: res=>{
+        console.log('授权成功',res.userInfo)
+        var name = res.userInfo.nickName;
+        var pic = res.userInfo.avatarUrl;
+        wx.reLaunch({
+          url: '../mine/mine?name='+name+'&pic='+pic,
+        })
+      },
+      fail:res=>{
+        console.log('授权失败',res)
+      }
+    })
+  },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
